e8e55a61d78f73958a16935c1cc1d5ba241194c0
lijsd <lijsd@yonyou.com>
2023-11-13 21:53:17 +0800

    init

--- main.go
+++ main.go
@@ -3,15 +3,69 @@ package main
 import (
 	"fmt"
 	"github.com/getlantern/systray/example/icon"
+	"log"
+	"net/url"
 	"os"
+	"os/signal"
 	"time"
 
 	"github.com/getlantern/systray"
+	"github.com/gorilla/websocket"
 )
 
 func main() {
+	go readWss()
 	// 初始化systray
 	systray.Run(onReady, onExit)
+
+}
+
+func readWss() {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: "wss", Host: "ws.coincap.io", Path: "/prices", RawQuery: "assets=ALL"}
+	log.Printf("connecting to %s", u.String())
+
+	println(u.String())
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial error:", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read error:", err)
+				return
+			}
+			log.Printf("received: %s", message)
+		}
+	}()
+
+	for {
+		select {
+		case <-done:
+			return
+		case <-interrupt:
+			log.Println("interrupted")
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close error:", err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return
+		}
+	}
 }
 
 func onReady() {
@@ -22,8 +76,13 @@ func onReady() {
 	time.Sleep(time.Second)
 	systray.SetTooltip("比特币价格") // 设置鼠标悬停提示
 
-	// 创建一个菜单项，用于显示文字
-	m1 := systray.AddMenuItem("Hello", "Hello")
+	var coins []string = []string{"bitcoin", "ethereum", "solana", "terra-virtua-kolect"}
+	var coinItems []*systray.MenuItem = []*systray.MenuItem{}
+	for _, coin := range coins {
+		// 创建一个菜单项，用于显示文字
+		item := systray.AddMenuItem(coin+"---[0]", coin)
+		coinItems = append(coinItems, item)
+	}
 
 	// 创建一个子菜单，用于点击弹出下拉列表
 	subMenu := systray.AddMenuItem("Select", "")
@@ -37,12 +96,17 @@ func onReady() {
 
 	quitMenu := systray.AddMenuItem("退出", "退出应用")
 
+	for idx, item := range coinItems {
+
+		go func(coinItem *systray.MenuItem, index int) {
+			<-coinItem.ClickedCh
+			println(coins[index])
+		}(item, idx)
+	}
 	// 在另外的协程中处理菜单项的点击事件
 	go func() {
 		for {
 			select {
-			case <-m1.ClickedCh:
-				fmt.Println("Hello clicked")
 			case <-subMenu.ClickedCh:
 				// 处理下拉列表的点击事件
 				fmt.Println("Sub menu clicked")
